c622fdd7970a0e001a5be77c4f1f7c515e99dbec
uux002 <uux002@gmail.com>
2018-03-31 19:18:49 +0800

    Init Project

---
+++ Assets/Scripts/Logic/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float moveSpeed;
+    public Joystick joystick;
+    public Transform playerMesh;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Vector3 moveVector = (transform.right * joystick.Horizontal + transform.forward * joystick.Vertical).normalized;
+        transform.Translate(moveVector * moveSpeed * Time.deltaTime);
+
+        playerMesh.Rotate(new Vector3(0, 20, 0));
+    }
+}
